a1e9a887140174fd0773b619e17ce4e70bb9b399
russian <dominusfinem@gmail.com>
2024-05-16 11:00:07 -0700

    Initial commit

---
+++ src/main/java/com/ixume/armorhud/ArmorHud.java
@@ -0,0 +1,17 @@
+package com.ixume.armorhud;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorHud extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
